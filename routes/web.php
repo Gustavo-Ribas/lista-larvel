65349319443df07f00b46ceb5b21da5fae2498fa
Gustavo <gustavoribas286@gmail.com>
2026-09-08 17:34:15 -0300

    ativ 3 alunocontroller e 7 metodos do crud

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AlunoController;
+
+Route::resource('alunos', AlunoController::class);
 
 Route::get('/', function () {
     return view('welcome');
