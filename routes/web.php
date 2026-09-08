d6989b602bbbfb76874023b1e3fb4f174303b77e
Gustavo <gustavoribas286@gmail.com>
2026-09-08 16:18:05 -0300

    atv1: rotas /sobre /alunos /contato

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+route :: get('/sobre', function(){
+    return 'Esta é a pagina Sobre.';
+});
+route :: get('/alunos', function(){
+    return 'Lista de alunos.';
+});
+route :: get('/contato', function(){
+    return 'Fale conosco.';
+});
