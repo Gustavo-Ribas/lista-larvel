9fd1a1c7e6adde1296c1fa5d69c620db9b7745b6
Gustavo <gustavoribas286@gmail.com>
2026-09-09 13:21:10 -0300

    atv18 isntalacao e configurar laravel breeze

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AlunoController;
-
-Route::resource('alunos', AlunoController::class);
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-route :: get('/sobre', function(){
-    return 'Esta é a pagina Sobre.';
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-route :: get('/contato', function(){
-    return 'Fale conosco.';
-});
-Route::get('/cursos/{id}/alunos', [AlunoController::class, 'alunosDoCurso']);
+
+require __DIR__.'/auth.php';
