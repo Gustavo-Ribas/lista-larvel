466b0600fa0e144360b4b1b3c3638abcda288513
Gustavo <gustavoribas286@gmail.com>
2026-09-08 23:29:25 -0300

    atv14 formulario cadastro alunos

--- resources/views/alunos/create.blade.php
+++ resources/views/alunos/create.blade.php
@@ -3,7 +3,9 @@
     <h1>Cadastrar Aluno</h1>
     <form method="POST" action="{{ route('alunos.store') }}">
         @csrf
-        <input type="text" name="nome" placeholder="Nome">
+        <input type="text" name="nome" placeholder="Nome" value="{{ old('nome') }}"><br>
+        <input type="text" name="curso" placeholder="Curso" value="{{ old('curso') }}"><br>
+        <input type="email" name="email" placeholder="Email" value="{{ old('email') }}"><br>
         <button type="submit">Salvar</button>
     </form>
 @endsection
